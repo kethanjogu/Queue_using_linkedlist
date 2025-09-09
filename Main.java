fe2c2d04ef79a939ccf68cf1a97e8ead673b7308
kethan <jsaikethan@gmail.com>
2025-09-09 18:37:10 +0530

    Initial commit - Queue implementation

---
+++ Main.java
@@ -0,0 +1,14 @@
+// Demo
+public class Main {
+    public static void main(String[] args) {
+        QueueLL q = new QueueLL();
+
+        q.enqueue(5);
+        q.enqueue(15);
+        q.enqueue(25);
+
+        System.out.println("Front element: " + q.peek()); // 5
+        System.out.println("Dequeued: " + q.dequeue());  // 5
+        System.out.println("Front element: " + q.peek()); // 15
+    }
+}
---
+++ queue_linkedlist.java
@@ -0,0 +1,56 @@
+
+class QueueLL {
+    private Node front, rear;
+    private int size;
+
+    public QueueLL() {
+        front = rear = null;
+        size = 0;
+    }
+
+    // Enqueue
+    public void enqueue(int item) {
+        Node newNode = new Node(item);
+        if (rear == null) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+        size++;
+    }
+
+    // Dequeue
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
+            return -1;
+        }
+        int item = front.data;
+        front = front.next;
+
+        if (front == null) {
+            rear = null;
+        }
+        size--;
+        return item;
+    }
+
+    // Peek
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
+            return -1;
+        }
+        return front.data;
+    }
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
+
