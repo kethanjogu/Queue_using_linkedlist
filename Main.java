dc5162dbe86e90dc2c4b3108f52e5eac0febc3cd
kethan <jsaikethan@gmail.com>
2025-09-09 19:32:23 +0530

    display function added

--- Main.java
+++ Main.java
@@ -7,8 +7,10 @@ public class Main {
         q.enqueue(15);
         q.enqueue(25);
 
-        System.out.println("Front : " + q.peek()); // 5
-        System.out.println("Dequeued: " + q.dequeue());  // 5
-        System.out.println("Front : " + q.peek()); // 15
+    
+        q.display(); 
+
+        System.out.println("Dequeued: " + q.dequeue()); // 10
+        q.display(); 
     }
 }
